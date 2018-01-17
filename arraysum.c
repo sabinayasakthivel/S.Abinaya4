83f380d59d6b160b97e082ed0cdc1c5e96174c9e
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-17 06:22:10 +0530

    Create arraysum.c

---
+++ arraysum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a[n] k;
+    scanf("%d,%d",&n,&k);
+    int i;
+    for(i=1;i<k;i++)
+    {for(i=1;i<=n;i++)
+    count=i+count;
+    }
+    printf("sum of natural numbers are:%d",count);
+    return 0;
+}
