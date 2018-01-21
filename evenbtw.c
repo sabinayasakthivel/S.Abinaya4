83f941d053d5fc68acb8d272a9321bbb3358bf3a
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-21 10:13:52 +0530

    Create evenbtw.c

---
+++ evenbtw.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,i,rem;
+	printf("enter the numbers");
+	scanf("%d,%d",&a,&b);
+	for(i=a;i<=b;i++)
+	{
+	rem=i%2;
+	if (rem==0)
+	printf("%d is a even number",i);
+	}
+	
+	return 0;
+}
