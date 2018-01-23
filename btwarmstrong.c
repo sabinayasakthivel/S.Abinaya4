24af560bb9b2fcc6007ca4fd6c15fc53ebb013b1
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-23 21:32:44 +0530

    Create btwarmstrong.c

---
+++ btwarmstrong.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a, b, i, temp1, temp2, remainder, n = 0, result = 0;
+
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &a, &b);
+    printf("Armstrong numbers between %d an %d are: ", a, b);
+
+    for(i = a+ 1; i <=b; ++i)
+    {
+        temp2 = i;
+        temp1 = i;
+
+       
+        while (temp1 != 0)
+        {
+            temp1 /= 10;
+            ++n;
+        }
+
+       
+        while (temp2 != 0)
+        {
+            remainder = temp2 % 10;
+            result += pow(remainder, n);
+            temp2 /= 10;
+        }
+
+       
+        if (result == i) {
+            printf("%d ", i);
+        }
+
+       
+        n = 0;
+        result = 0;
+
+    }
+    return 0;
+}
