c63e9d2d8fdb97d1ee39dd170dd72ab351e384af
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-24 17:00:50 +0530

    Create swapof two.c

---
+++ swapof two.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+int a,b;
+printf("enter the numbers");
+scanf("%d%d",&a,&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swapping a&b values are%d,%d",a,b);
+
+
+	return 0;
+}
