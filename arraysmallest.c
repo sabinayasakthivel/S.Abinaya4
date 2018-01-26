5bdd75bb7e88d82aa1ca580b8363962bb8cb955b
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-27 03:25:38 +0530

    Create arraysmallest.c

---
+++ arraysmallest.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	
+int a[20],i,j,n;
+printf("enter the number of values");
+scanf("%d",&n);
+printf("enter the elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0+1;i<n;i++)
+{
+if(a[0]>a[i])
+a[0]=a[i];
+}
+
+printf("smallest element of an arrayis %d",a[0]);
+
+return 0;
+
+}
+	
+
