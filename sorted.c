f58f32e5e5e9abe7783a3f2271d529078109ba7e
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-27 04:02:47 +0530

    Update sorted.c

--- sorted.c
+++ sorted.c
@@ -3,12 +3,14 @@ int main()
 {
 int a[20],i,j,n,t;
 printf("enter the number of values");
-scanf("%d",&n)
+scanf("%d",&n);
 printf("enter the values");
-for(i=0;i<n;i++);
+for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
+for(i=0;i<n-1;i++)
+{
 for(j=i+1;j<n;j++)
 {
 if(a[i]>a[j])
@@ -16,10 +18,14 @@ if(a[i]>a[j])
 t=a[i];
 a[i]=a[j];
 a[j]=t;
-}}
+}
+}   
+}
 printf("sorted elemete are:");
 for(i=0;i<n;i++)
 {
 printf("%d",a[i]);
 }
 }
+
+    
