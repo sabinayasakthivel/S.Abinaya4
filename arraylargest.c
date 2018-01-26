2df4d0449e1b4c6370dd08d1e1f17b3af5ba8a9a
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-27 03:05:21 +0530

    Create arraylargest.c

---
+++ arraylargest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	
+int a[20],i,j,n;
+printf("enter the number of values");
+scanf("%d",&n);
+printf("enter the elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0+1;i<n;i++)
+{
+if(a[0]<a[i])
+{
+a[0]=a[i];
+}
+}
+printf("largest element of an arrayis %d",a[0]);
+return 0;
+
+}
+	
