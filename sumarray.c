990ca0ffea786cddff07a88ba3ed679aef23f3c3
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-27 06:38:11 +0530

    Create sumarray.c

---
+++ sumarray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int a[20],i,n,sum=0,avg;
+printf("enter the number of values");
+scanf("%d",&n);
+printf("enter the values");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+	{
+	sum=sum+a[i];
+	}
+	printf("sum is %d:",sum);
+	
+	return 0;
+}
