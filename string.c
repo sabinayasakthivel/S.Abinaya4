75bf8733e19ef052d3a0f2dd5576204802fc488d
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-27 05:40:02 +0530

    Create new file

---
+++ string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char a[20],k,i;
+    printf("enter the string");
+ 
+    scanf("%s",a);
+    printf("enter the number");
+    scanf("%s",&k);
+    for(i=1;i<=k;i++)
+    {
+    printf("string is %s",a);
+    }
+    return 0;
+}
+    
