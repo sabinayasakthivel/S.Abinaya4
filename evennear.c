9312cb4bc70c9423cc8cf07edb877f1c579c1c59
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-28 16:49:50 +0530

    Create evennear.c

---
+++ evennear.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,s,t;
+printf("enter the number");
+scanf("%d",&a);
+if((n%2)==0)
+{
+s=a+2;
+t=a-2;
+printf("the nearest even number is:%d %d",s,t);
+}
+else
+{s=a+1;t=a-1;
+printf("the nearest even number is:%d %d",s,t);
+}
+}
