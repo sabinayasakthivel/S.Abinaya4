2a174b3bc4229a2497dfd52e107489256e3a65f7
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-01-31 17:00:31 +0530

    Create alpha123.c

---
+++ alpha123.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+printf("enter the number");
+scanf("%d",&ch);
+if((ch>a &&ch>=z) &&(ch>A && ch<=Z))
+printf("it is alphapet");
+elseif
+printf("it is number");
+else
+printf("yes,contains number and alphapet);
+}
