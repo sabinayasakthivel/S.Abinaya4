228009e3956ae65577cbf6a659e15ec5a22f8f6d
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-02-04 22:16:43 +0530

    Update alpha123.c

--- alpha123.c
+++ alpha123.c
@@ -6,7 +6,7 @@ printf("enter the number");
 scanf("%d",&ch);
 if((ch>a &&ch>=z) &&(ch>A && ch<=Z))
 printf("it is alphapet");
-elseif
+elseif(ch<='9')
 printf("it is number");
 else
 printf("yes,contains number and alphapet);
