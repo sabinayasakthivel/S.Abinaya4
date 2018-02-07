884c981308c0af21f8ceef7dd27ca341e5209524
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-02-07 21:19:57 +0530

    Create repitionsofk.c

---
+++ repitionsofk.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i ,a[50],c;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<=n;i++)
+{
+if(a[i]==k)
+c++;
+}
+printf("number of repitions:%d",c);
+}
