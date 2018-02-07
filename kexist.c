ce849dad4b1c72e4f7de3fc043727e985a0fb12d
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-02-07 21:10:56 +0530

    Create kexist.c

---
+++ kexist.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i ,a[50],c;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<n;i++)
+{
+if(a[i]==k)
+c++;
+}
+if(c>0)
+printf("yes");
+else
+printf("no");
+}
