421d8534ff0721ab92623b0d6b3c51e19b80c68f
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-02-14 20:31:11 +0530

    Create withoutstrcmp.c

---
+++ withoutstrcmp.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int i,a[10],b[10],n;
+printf("enter the number of values to print in a matrix");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the number of values to print in b matrix");
+{
+scanf("%d",&n);
+}
+for(i=0;i<n;i++)
+scanf("%d",&b[i]);
+if(a[i]=b[i])
+printf("the string is same");
+else
+printf("the string is same");
+return 0;
+}
+
+
