cf4fd04afa8d94cbcb5ef166d0a3e27c27192398
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-02-18 10:03:49 +0530

    Create laptop3.c

---
+++ laptop3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,a[20],k;
+printf("enter the number of values :");
+scanf("%d",&n);
+printf("enter the number");
+scanf("%d",&k);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<k;i++)
+printf("%d",&n);
+return 0;
+}
