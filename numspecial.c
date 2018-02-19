e0d66f3c5b47f8dafb2207daca98b16595efc4fd
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-02-19 16:59:42 +0530

    Create numspecial.c

---
+++ numspecial.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[50];
+int i,l,b=0,n=0,s=0;
+gets(a);
+l=strlen(a);
+printf("%d",l);
+for(i=0;i<l;i++)
+{
+if(a[i]>='0'&&a[i]<='9')
+{
+n++;
+}
+else if((a[i]>='A' && a[i]<='Z') || (a[i]<='a' && a[i]<='z'))
+{
+b++;
+}
+else
+{
+s++;
+}}
+printf("number of special characters are:%d",b);
+return 0;
+}
