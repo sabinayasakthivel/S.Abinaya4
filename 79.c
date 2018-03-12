5fc40988153b856e5163cdb22932da9d538ef987
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-03-12 16:10:01 +0530

    Update and rename 1.c to 79.c

--- 79.c
+++ 79.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 void main()
 {
-int a,b,i,c;
+int a,b,i,c,k=0,d;
 scanf("%d%d",&a,&b);
 c=a*b;
 for(i=1;i<c;i++)
 {
-d=pow(i,i);
+d=i*i;
+if(c==d)
+  k++;
 }
-if(c==d);
+  if(k==1)
 printf("yes");
 else
 printf("no");
