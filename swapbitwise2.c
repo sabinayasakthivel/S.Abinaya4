cf6340b183400c7cfeef1a8bc8025bfb17e6a7bb
sabinayasakthivel <31499373+sabinayasakthivel@users.noreply.github.com>
2018-03-19 15:50:57 +0530

    Create swapbitwise2.c

---
+++ swapbitwise2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{int a,b;
+printf("enter the numbers");
+scanf("%d%d",&a,&b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("after swapping a&b values are%d %d",a,b);
+return 0;
+}
